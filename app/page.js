5bc3eeab964347485484cd77d44bd160e585b74e
Kuldeep <kuldeepmer11@gmail.com>
2023-11-22 22:11:38 +0530

    homepage-2done

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
 import Navbar from '@/src/components/Navbar/Navbar';
 import './page.css'
 import Hero from '@/src/components/Hero/Hero';
+import BrandingVedio from '../src/components/BrandingVedio/BrandingVedio';
+import WhatWeDo from '@/src/components/What we do/WhatWeDo';
 export default function Home() {
 
   return (
     <div className='app'>
      <Navbar/>
      <Hero/>
+     <BrandingVedio/>
+     <WhatWeDo/>
     </div>
   );
 }
